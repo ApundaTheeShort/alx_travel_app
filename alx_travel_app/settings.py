58056fed6a352ca2e7ef78c74f216b9001efb2af
ApundaTheeShort <apundawyclife333@gmail.com>
2026-06-01 19:25:18 +0300

    Add Django REST framework and related packages to settings and requirements

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
+    'rest_framework',
+    'drf-yasg',
+    'corsheaders',
+    'drf_spectacular',
     'listings',
-    ''
 ]
 
 MIDDLEWARE = [
